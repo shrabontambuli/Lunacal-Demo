1e42485c35eb9996f1786f333e9516df28c0c4b4
Shrabon Tambuli <shrabonsa@gmail.com>
2024-09-05 03:08:58 +0600

    add bar in tab section

--- src/Components/Content/Left_first_Content.jsx
+++ src/Components/Content/Left_first_Content.jsx
@@ -21,6 +21,12 @@ const Left_first_Content = () => {
                 <p>Hello! I’m Dave, your sales rep here from Salesforce. I’ve been working at this awesome company for 3 years now.</p>
                 <p className='mt-6'>I was born and raised in Albany, NY& have been living in Santa Carla for the past 10 years my wife Tiffany and my 4 year old twin daughters- Emma and Ella. Both of them are just starting school, so my calender is usually blocked between 9-10 AM. This is a...</p>
             </div>
+            <div className='absolute centerP'>
+                <img src="/icons/Frame 1000002250.png" alt="" />
+            </div>
+            <div className='absolute centerR'>
+                <img src="/icons/bar.png" alt="" />
+            </div>
         </div>
     );
 };
